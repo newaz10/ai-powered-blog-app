7fa034e0fcd62601bafc106f96511dc29c93eb28
MH Newaz <mh.newaz001@gmail.com>
2026-06-15 21:07:00 +0600

    fix: changed the footer content

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
         </div>
       </div>
       <p className="py-4 text-center text-sm md:text-base text-gray-600">
-        Copyright 2026 ©️ QuickBlog Developed by Newaz - All Right Reserverd.
+        © 2026 QuickBlog. Developed by MH Newaz. All rights reserved.
       </p>
     </div>
   );
